e79e7a83659396218009e4518fa924ccdb06a726
huynhsonca@gmail.com <cahs@evolable.asia>
2014-06-12 18:23:54 +0700

    them chuc nang liet ke danh sach doanh nghiep

--- Config/routes.php
+++ Config/routes.php
@@ -1,4 +1,5 @@
 <?php
+Router::connect('/admin/doanhnghiep/lietke/*',array('plugin'=>'Admin','controller'=>'Doanhnghieps','action'=>'lietke'));
 Router::connect('/admin/doanhnghiep/lietke',array('plugin'=>'Admin','controller'=>'Doanhnghieps','action'=>'lietke'));
 Router::connect('/admin/doanhnghiep/themmoi',array('plugin'=>'Admin','controller'=>'Doanhnghieps','action'=>'themmoi'));
 CakePlugin::routes();
--- Plugin/Admin/Controller/DoanhNghiepsController.php
+++ Plugin/Admin/Controller/DoanhNghiepsController.php
@@ -5,7 +5,8 @@ App::uses('AdminAppController', 'Admin.Controller');
  *
  */
 class DoanhNghiepsController extends AdminAppController {
-
+   public $components=array('Paginator');
+    public $helpers=array('Paginator');
 /**
  * Scaffold
  *
@@ -13,7 +14,16 @@ class DoanhNghiepsController extends AdminAppController {
  */
 //	public $scaffold;
 	public $layout = 'admin/default';
+    public $paginate =array(
+        'limit'=>20,
+//        'order'=>array(
+//            'DoanhNghiep.colMa'=>'asc'
+//        )
+    );
 	public function lietke(){
+        $this->Paginator->settings=$this->paginate;
+        $data=$this->Paginator->paginate('DoanhNghiep');
+        $this->set('data',$data);
     }
 	public function themmoi(){}
 }
