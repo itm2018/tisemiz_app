bcdaee7ac9ad72bd58227d94d16bad62c0b675f8
huynhsonca@gmail.com <cahs@evolable.asia>
2014-08-07 08:34:01 +0700

    change routes

--- Plugin/Admin/Controller/AdminAppController.php
+++ Plugin/Admin/Controller/AdminAppController.php
@@ -6,15 +6,6 @@ class AdminAppController extends AppController {
 
 	public $helpers = array('Html', 'Form');
 
-	public function beforeFiler() {
-		parent::beforeFilter();
-		$this->Security->validatePost = false;
-		$this->Security->unlockedActions = array('dulieugiamsatmoitruong');
-		$this->Security->blackHoleCallback = 'blackhole';
-	}
-
-	public function blackhole() {
-		throw new NotFoundException('Trang bạn đang tìm không tồn tại');
-	}
+	
 
 }
